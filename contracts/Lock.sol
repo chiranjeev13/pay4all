b7e1ec9f6d3c78e2fb1a1e37d09eaa87030f483a
Gautham Krishna <90638995+gauthking@users.noreply.github.com>
2023-02-17 00:40:45 +0530

    added some stuff in contract

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -2,53 +2,184 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 contract dClap {
-    
-    address tokenAddress = 
-   mapping(uint256 => mapping (address => Society_members)) Society;
-    struct Society_members
-    {
-        uint256 resident_id;
+    address immutable tokenAddress = 0x0000000000000000000000000000000000001010;
+
+    struct Society_members {
+        uint256 member_id;
+        uint256 society_id;
         string name;
         uint256 phone_no;
         bool active;
+        string role; // resident, coordinators, workers
     }
-    mapping(address => Workers) worker;
-    struct Workers
-    {
+
+    struct Society {
+        address society_deployer;
+        uint256 society_id;
+        string society_name;
+        string employer_name;
+        string purpose;
+        uint256 period_of_operation;
+        bool active; // to check whether the society is under operation or has discontinued in between.
+    }
+
+    struct Workers {
         uint256 worker_id;
+        address worker_address;
         string name;
         uint256 phone_no;
+        uint256 balance;
         bool active;
-    }
-    function addSociety(uint256 resident_id,string memory name,uint256 phone_no) public {
-        uint256 society_id =0;
-        Society[society_id][msg.sender]=Society_members(resident_id,name,phone_no,true);//atleast one resident is reqd to register
-        society_id++;
-
-    }
-    function addMembers_to_society(uint256 society_id,address resident_address,uint256 resident_id,string memory name,uint256 phone_no) public 
-    {
-        require(Society[society_id][resident_address].active == false,"Already in society");
-        Society[society_id][resident_address]=Society_members(resident_id,name,phone_no,true);
-
-    }
-    function deleteMembers_from_society(uint256 society_id) public 
-    {
-        require(Society[society_id][msg.sender].active == true,"not from this society");
-        Society[society_id][msg.sender].active = false;
+        bool inService;
     }
 
-    function registerWorkers(uint256 worker_id,string memory name,uint256 phone_no)public 
-    {
-        require(worker[msg.sender].active==false,"Already registered");
-        worker[msg.sender]=Workers(worker_id,name,phone_no,true);
-
+    struct Residents {
+        uint256 resident_id;
+        address resident_address;
+        string name;
+        uint256 phone_no;
+        uint256 balance;
+        bool active;
+        bool inService;
     }
-    function pay_to_worker(uint256 society_id,uint256 val,address workerAddress) public
-    {
-        require(Society[society_id][msg.sender].active == true,"Not a valid resident");
-        require(worker[workerAddress].active==true,"Not a valid worker");
 
+    mapping(uint256 => mapping(address => Society_members)) SocietyMapper;
+    mapping(address => Workers) worker;
+    mapping(address => Residents) resident;
+
+    Workers[] public workersArray;
+    Residents[] public residentsArray;
+
+    mapping(address => Society) public societyLogs;
+    Society[] public societies;
+
+    uint256 public workerCount;
+    uint256 public residentCount;
+    uint256 public societyCount;
+
+    function registerForSociety(
+        string memory society_name,
+        string memory employer_name,
+        string memory purpose,
+        uint256 period_of_operation
+    ) public {
+        societies.push(
+            Society(
+                msg.sender,
+                societyCount,
+                society_name,
+                employer_name,
+                purpose,
+                period_of_operation,
+                true
+            )
+        );
+        societyLogs[msg.sender] = societies[societyCount];
+        societyCount++;
     }
+
+    // function addSociety(
+    //     uint256 resident_id,
+    //     string memory name,
+    //     uint256 phone_no
+    // ) public {
+    //     Society[society_id][msg.sender] = Society_members(
+    //         resident_id,
+    //         name,
+    //         phone_no,
+    //         true
+    //     ); //atleast one resident is reqd to register
+    //     society_id++;
+    // }
+
+    function addMembersToSociety(
+        uint256 society_id,
+        address member_address,
+        uint256 member_id,
+        uint256 phone_no,
+        string memory name,
+        string memory role
+    ) public {
+        require(
+            SocietyMapper[society_id][member_address].active == false,
+            "Already in society"
+        );
+        SocietyMapper[society_id][member_address] = Society_members(
+            member_id,
+            society_id,
+            name,
+            phone_no,
+            true,
+            role
+        );
+    }
+
+    function deleteMembers_from_society(uint256 society_id) public {
+        require(
+            SocietyMapper[society_id][msg.sender].active == true,
+            "not from this society"
+        );
+        SocietyMapper[society_id][msg.sender].active = false;
+    }
+
+    function registerWorkers(
+        uint256 worker_id,
+        string memory name,
+        uint256 phone_no
+    ) public {
+        require(worker[msg.sender].active == false, "Already registered");
+        worker[msg.sender] = Workers(
+            worker_id,
+            msg.sender,
+            name,
+            phone_no,
+            0,
+            true,
+            false
+        );
+        workersArray.push(
+            Workers(worker_id, msg.sender, name, phone_no, 0, true, false)
+        );
+        workerCount++;
+    }
+
+    function registerResidents(
+        uint256 resident_id,
+        string memory name,
+        uint256 phone_no
+    ) public {
+        require(resident[msg.sender].active == false, "Already registered");
+        resident[msg.sender] = Residents(
+            resident_id,
+            msg.sender,
+            name,
+            phone_no,
+            0,
+            true,
+            false
+        );
+        residentsArray.push(
+            Residents(resident_id, msg.sender, name, phone_no, 0, true, false)
+        );
+        residentCount++;
+    }
+
+    // function pay_to_worker(
+    //     uint256 society_id,
+    //     uint256 val,
+    //     address workerAddress
+    // ) public {
+    //     require(
+    //         Society[society_id][msg.sender].active == true,
+    //         "Not a valid resident"
+    //     );
+    //     require(worker[workerAddress].active == true, "Not a valid worker");
+    //     require(
+    //         IERC20(tokenAddress).balanceOf(msg.sender) > msg.value,
+    //         "Not enough to balance to make this transaction"
+    //     );
+    //     // IERC20(0x0000000000000000000000000000000000001010)
+    // }
 }
